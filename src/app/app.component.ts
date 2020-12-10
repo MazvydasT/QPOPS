310f5bb4f9769a4b416014a46fe4e6f0573953c8
Mazvydas Tadaravicius <mazvydas.tadaravicius@gmail.com>
2020-12-10 21:08:36 +0000

    Updated browser not supported page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
   version = {
     major: 1,
     minor: 11,
-    patch: 7
+    patch: 8
   };
   howToLinks = [
     { name: `Filtering`, link: `assets/videos/filtering.mp4` },
